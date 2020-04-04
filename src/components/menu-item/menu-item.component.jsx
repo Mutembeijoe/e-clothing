43cf932bd5bf625cbb643c5df27708943cb7a5f4
mutembeijoe <mutembeijoe@gmail.com>
2020-04-04 20:15:33 +0300

    adds more styling and effects to the menu item

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -4,7 +4,13 @@ import './menu-item.styles.scss'
 
 const MenuItem = ({title, imageUrl, size})=>{
     return (
-    <div className={`menu-item ${size}`} style={{ backgroundImage:`url(${imageUrl})`}}>
+    <div className={`menu-item ${size}`}>
+        <div
+            className='background-image'
+            style={{
+            backgroundImage: `url(${imageUrl})`
+            }}
+        />
         <div className='content'>
             <div className='title'>{title}</div>
             <span className='subtitle'>SHOP NOW</span>
